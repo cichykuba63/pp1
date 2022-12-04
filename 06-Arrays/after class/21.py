1f411197e7ad87a9c52ccb476f0c6b249fb5ff92
cichykuba63 <jakub.slowiak9@o2.pl>
2022-12-04 18:41:28 +0100

    homework

--- 06-Arrays/after class/21.py
+++ 06-Arrays/after class/21.py
@@ -1,4 +1,19 @@
 arr = [1, 5, 20, 100, 500, 620, 920, 999]
 
+print()
+print("-" * 41)
 
-print(arr)
+for number in arr:
+    if number == arr[len(arr) - 1]:
+        print(f"| {number}|", end = "")
+        break
+    if number < 10:
+        print(f"|   {number}", end = "")
+    elif number < 100 and number >= 10:
+        print(f"|  {number}", end = "")
+    else:
+        print(f"| {number}", end = "")
+
+print() 
+print("-" * 41)
+print()
